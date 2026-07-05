3d855b5602b345a24d55650e53a953ccd0ea3a6b
YanGi-Kon <boburbahromvic@gmail.com>
2026-07-05 13:46:44 +0500

    chore: bust media stage login cache

--- public/js/fix.js
+++ public/js/fix.js
@@ -204,7 +204,7 @@ function isSanegLoginActive(){
   if (document.getElementById('sanegLoginBlankRightScript')) return;
   const script = document.createElement('script');
   script.id = 'sanegLoginBlankRightScript';
-  script.src = 'js/saneg-login-blank-right.js?v=blank1b';
+  script.src = 'js/saneg-login-blank-right.js?v=media1a';
   script.async = false;
   script.defer = true;
   document.head.appendChild(script);
