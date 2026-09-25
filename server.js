ed7e230b5565392c45807bcb45cd8242e258bbe1
YanGi-Kon <boburbahromvic@gmail.com>
2026-09-25 20:13:11 +0500

    chore(to): refresh period bridge for monthly home

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get("/", (_req, res, next) => {
 app.get("/modules/to.html", (_req, res, next) => {
   try {
     const html = readFileSync(toHtmlPath, "utf8");
-    const bridgeScript = '<script id="toPeriodSheetBridgeScript" src="/js/to-period-sheet-bridge.js?v=to-period-bridge12-final-documents"></script>';
+    const bridgeScript = '<script id="toPeriodSheetBridgeScript" src="/js/to-period-sheet-bridge.js?v=to-period-bridge13-analysis-home"></script>';
     const safeHtml = html.includes("toPeriodSheetBridgeScript")
       ? html
       : html.replace("</body>", `${bridgeScript}\n</body>`);
